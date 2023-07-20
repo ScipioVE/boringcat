77eb7a55d644147fa1862ea95f1279e042af4550
Manuel Matheus <manuelmatheus.arg@gmail.com>
2023-07-20 09:30:02 -0300

    add: NewBook View and Form Working

--- boringcat/settings.py
+++ boringcat/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'')
 MEDIA_URL  = '/images/'
