f9a8ece5eff8e81e8790be84ab24100a050f7cd2
Manuel Matheus <manuelmatheus.arg@gmail.com>
2023-07-21 12:49:44 -0300

    add: Delete , edit

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('Books', views.Index,name='Books'),
     path('Books/new_book', views.newBookForm,name='NewBook'),
     path('Books/edit_book', views.editBookForm,name='EditBook'),
+    path('delete_book/<int:id>', views.deleteBook, name='DeleteBook'),
+    path('Books/edit_book/<int:id>', views.editBookForm, name='EditBook'),
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
