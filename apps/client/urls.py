5c8ad997efaa99ab5036302729919b3b04e99685
Manuel Matheus <manuelmatheus.arg@gmail.com>
2023-07-26 04:23:33 -0300

    typo error

--- apps/client/urls.py
+++ apps/client/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
 urlpatterns = [
-    path('signup/', views.signupform , name='signup'),
+    path('signup/', views.signupform , name='Signup'),
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
