706a4aa933d5da25af2eaf931a84327bbcbeada3
Manuel Matheus <manuelmatheus.arg@gmail.com>
2023-08-02 02:03:54 -0300

    add: New urls

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -5,11 +5,14 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('', views.start,name='Home'),
     path('About', views.Aboutus,name='About'),
-    path('Books', views.Index,name='Books'),
-    path('Books/new_book', views.newBookForm,name='NewBook'),
-    path('Books/edit_book', views.editBookForm,name='EditBook'),
-    path('delete_book/<int:id>', views.deleteBook, name='DeleteBook'),
-    path('Books/edit_book/<int:id>', views.editBookForm, name='EditBook'),
-    path('Book/Post/<int:id>', views.post_detail, name='Post')
+    path('Admin/Books', views.Index,name='Books'),
+    path('Admin/Books/new_book', views.newBookForm,name='NewBook'),
+    path('Admin/Books/edit_book', views.editBookForm,name='EditBook'),
+    path('Admin/delete_book/<int:id>', views.deleteBook, name='DeleteBook'),
+    path('Admin/Books/edit_book/<int:id>', views.editBookForm, name='EditBook'),
+    path('Book/Post/<int:id>', views.post_detail, name='Post'),
+    path('Admin/Book/Post/New', views.postForm, name = 'PostForm'),
+    path('Admin/Book/post_edit/<int:id>', views.editPostForm, name= 'EditPost'),
+    path('Catalog', views.catalog, name= 'Catalog'),
     
 ] + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
